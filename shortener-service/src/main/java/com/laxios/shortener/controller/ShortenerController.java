fb61f237b200c378a3cfb8338191bf8754fc22b9
Laxman B <dev.laxmanb@gmail.com>
2025-08-18 23:57:54 +0000

    add shortener service

--- shortener-service/src/main/java/com/laxios/shortener/controller/ShortenerController.java
+++ shortener-service/src/main/java/com/laxios/shortener/controller/ShortenerController.java
@@ -1,17 +1,22 @@
 package com.laxios.shortener.controller;
 
 import com.laxios.shortener.dto.ShortenRequest;
+import com.laxios.shortener.service.ShortenerService;
+import lombok.Data;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Data
 @RestController
 @RequestMapping("/api/shorten")
 public class ShortenerController {
 
+    final ShortenerService shortenerService;
     @PostMapping
     public String shorten(@RequestBody ShortenRequest shortenRequest) {
+        shortenerService.processURL();
         System.out.println("Shorten called " + shortenRequest.getUrl());
         return "Shorten endpoint hit!";
     }
---
+++ shortener-service/src/main/java/com/laxios/shortener/service/ShortenerService.java
@@ -0,0 +1,12 @@
+package com.laxios.shortener.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShortenerService {
+
+    public void processURL() {
+        System.out.println("processing URL");
+    }
+}
