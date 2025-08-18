4e4b226716c913bcad0a671b7392f945eda266ee
Laxman B <dev.laxmanb@gmail.com>
2025-08-18 23:33:00 +0000

    init springboot

---
+++ shortener-service/src/main/java/com/laxios/shortener/ShortenerServiceApplication.java
@@ -0,0 +1,11 @@
+package com.laxios.shortener;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class ShortenerServiceApplication{
+
+    public  static void main(String args[]) {
+        System.out.println("Hello From SpringBoot");
+    }
+}
