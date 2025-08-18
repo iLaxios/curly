5feb33f4b6d2c9c59b73b9534df6547c8639f85c
Laxman B <dev.laxmanb@gmail.com>
2025-08-18 23:53:49 +0000

    add shortener request dto

--- shortener-service/src/main/java/com/laxios/shortener/controller/ShortenerController.java
+++ shortener-service/src/main/java/com/laxios/shortener/controller/ShortenerController.java
@@ -1,6 +1,8 @@
 package com.laxios.shortener.controller;
 
+import com.laxios.shortener.dto.ShortenRequest;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShortenerController {
 
     @PostMapping
-    public String shorten() {
-        System.out.println("Shorten called");
+    public String shorten(@RequestBody ShortenRequest shortenRequest) {
+        System.out.println("Shorten called " + shortenRequest.getUrl());
         return "Shorten endpoint hit!";
     }
 }
