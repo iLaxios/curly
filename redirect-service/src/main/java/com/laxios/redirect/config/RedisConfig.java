6e955f73cc655ccdb0b3d82e2aeee9e7161a5c90
Laxman B <dev.laxmanb@gmail.com>
2025-08-19 17:30:30 +0000

    Redis Config

---
+++ redirect-service/src/main/java/com/laxios/redirect/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.laxios.redirect.config;
+
+import com.laxios.redirect.UrlMapping;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, UrlMapping> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, UrlMapping> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        Jackson2JsonRedisSerializer<UrlMapping> serializer = new Jackson2JsonRedisSerializer<>(UrlMapping.class);
+        template.setValueSerializer(serializer);
+        template.setKeySerializer(new StringRedisSerializer());
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
