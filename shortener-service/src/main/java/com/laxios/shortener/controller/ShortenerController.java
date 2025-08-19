8335c7deacf78f351ca0c81a330b2c29812e94ab
Laxman B <dev.laxmanb@gmail.com>
2025-08-19 14:40:39 +0000

    url validation

--- shortener-service/src/main/java/com/laxios/shortener/controller/ShortenerController.java
+++ shortener-service/src/main/java/com/laxios/shortener/controller/ShortenerController.java
@@ -3,6 +3,7 @@ package com.laxios.shortener.controller;
 import com.laxios.shortener.dto.ShortenRequest;
 import com.laxios.shortener.service.ShortenerService;
 import lombok.Data;
+import org.apache.commons.validator.routines.UrlValidator;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,16 @@ public class ShortenerController {
     final ShortenerService shortenerService;
     @PostMapping
     public String shorten(@RequestBody ShortenRequest shortenRequest) {
-        shortenerService.processURL();
-        System.out.println("Shorten called " + shortenRequest.getUrl());
+        String inputUrl = shortenRequest.getUrl();
+
+        UrlValidator validator = new UrlValidator(
+                new String[] {"http","https"},
+                UrlValidator.ALLOW_LOCAL_URLS // remember to remove this;
+        );
+        if (!validator.isValid(inputUrl)) {
+            throw new IllegalArgumentException("Invalid URL format");
+        }
+
         return "Shorten endpoint hit!";
     }
 }
