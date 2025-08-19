b8cb862f6fca19e3d561420d1d83d056cf385ced
Laxman B <dev.laxmanb@gmail.com>
2025-08-19 18:22:58 +0000

    add urlmapping entity

---
+++ redirect-service/src/main/java/com/laxios/redirect/entity/UrlMapping.java
@@ -0,0 +1,25 @@
+package com.laxios.redirect.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UrlMapping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String originalUrl;
+    private String shortCode;
+}
