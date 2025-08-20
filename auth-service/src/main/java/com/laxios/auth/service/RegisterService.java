b9146c837dae91512a8e6b8e6fef1b6d1e9bc358
Laxman B <dev.laxmanb@gmail.com>
2025-08-20 19:30:19 +0000

    save user to db

--- auth-service/src/main/java/com/laxios/auth/service/RegisterService.java
+++ auth-service/src/main/java/com/laxios/auth/service/RegisterService.java
@@ -1,11 +1,28 @@
 package com.laxios.auth.service;
 
+import com.laxios.auth.model.User;
+import com.laxios.auth.repository.UserRepository;
+import lombok.Data;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@Data
 public class RegisterService {
 
-    public void registerUser(String user, String password) {
-        System.out.println("user and password recieve as " + user + " " + password);
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User registerUser(String username, String password) {
+
+        if (userRepository.existsByUsername(username)) {
+            throw new RuntimeException("Username already taken");
+        }
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password)); // hash password
+
+        return userRepository.save(user);
     }
 }
