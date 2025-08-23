85c907ade843f02ebf8519f3cab23632b93d7625
Laxman B <dev.laxmanb@gmail.com>
2025-08-23 17:27:32 +0000

    init user-analytics service

---
+++ user-analytics-service/src/main/java/com/laxios/useranalytics/UserAnalyticsApplication.java
@@ -0,0 +1,12 @@
+package com.laxios.useranalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAnalyticsApplication.class, args);
+    }
+}
